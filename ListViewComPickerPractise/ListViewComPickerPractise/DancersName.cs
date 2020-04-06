545c7f081b09d75ae3addc272d950bd648450924
sidhuraj <mr.prvsantosh@rediffmail.com>
2020-04-06 16:48:21 +0530

    first commit

---
+++ ListViewComPickerPractise/ListViewComPickerPractise/DancersName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListViewComPickerPractise
+{
+    public class DancersName
+    {
+        public string DancersPhoto { set; get; }
+        public string Dancerscountry { set; get; }
+        public string Dancersuniverse { set; get; }
+    }
+}
---
+++ ListViewComPickerPractise/ListViewComPickerPractise/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewComPickerPractise
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            List<DancersName> objDancers = new List<DancersName>();
+
+            objDancers.Add(new DancersName { DancersPhoto = "Michael.jpg", Dancerscountry = "USA", Dancersuniverse = "world" });
+            objDancers.Add(new DancersName { DancersPhoto = "shakria.jpg", Dancerscountry = "spanish", Dancersuniverse = "world" });
+            objDancers.Add(new DancersName { DancersPhoto = "prabhu.jpg", Dancerscountry = "indian", Dancersuniverse = "world" });
+            objDancers.Add(new DancersName { DancersPhoto = "hrithik.jpg", Dancerscountry = "indian", Dancersuniverse = "world" });
+
+            lblDanersList.ItemsSource = objDancers;
+
+        }
+    }
+}
